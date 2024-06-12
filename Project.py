9954bc461e941f0937fc41f3884904cd16afa5ff
Rishi10898 <168684167+Rishi10898@users.noreply.github.com>
2024-06-13 10:29:11 +1200

    wsc

--- Project.py
+++ Project.py
@@ -1,3 +1,3 @@
-#Greetings and Rules of the Alus game
-# Names of the surburbs in Aucxkland district
+#Greetings and Rules of the Alus game 
+# The atlus game starts from here
 # Conclusion and restarting of the game
