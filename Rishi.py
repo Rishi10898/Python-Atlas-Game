066e67aae86e5d99e45e70dc9df76a8b4956f480
WSCWNeel <159732843+WSCWNeel@users.noreply.github.com>
2024-06-28 13:25:02 +1200

    .

--- Rishi.py
+++ Rishi.py
@@ -1,3 +1,5 @@
+<<<<<<< HEAD
+=======
 #Greetings and Rules of the Alus game 
 # Task 1 
 def intro():
@@ -9,24 +11,165 @@ def rule():
     Rule_3 = print("You should start the name with the ending letter of the previous word")
 def rule_1():
     Rules_4 = input("Did you understand the rules of this Atlus game?")
-    if Rules_4 == "no":
+    if Rules_4 == "yes" or " yes" or "YES" or " YES":
+        print("That's good ! You have a good memory") 
+    elif Rules_4 == "":
+        print("Did you understand the rules?")
+    else: 
         print(rule()) 
-    else:
-        Rules_4 == "yes"
-        print()    
-Places_in_Auckland = ["Albany"] and ["Alfriston"] and ["Arch Hill"] and ["Auckland CBD"] and ["Avondale"] and ["Balmoral"] and ["Bayswater"] and ["Bayview"] and ["Beach Haven"] and ["Belmont"] and ["Birkdale"] and ["Birkenhead", "Burswood"] and ["Blockhouse Bay", "Browns Bay"] and ["Botany Downs"] and ["Botany"] and ["Bucklands Beach"] and ["Campbells Bay","Castor Bay", "Cockle Bay"] and ["Chatswood"] and ["Clendon Park","Clover Park"] and ["Conifer Grove"] and ["Dannemora"] and ["Devonport"] and ["Drury"] and ["East Tamaki"] and ["East Tamaki Heights"] and ["Eastern Beach"] and ["Eden Terrace","Ellerslie"] and ["Eden Valley"] and ["Epsom"] and ["Fairview Heights"] and ["Farm Cove"] and ["Favona"] and ["Flat Bush"] and ["Forrest Hill"] and ["Freemans Bay"] and ["Gardens"] and ["Glen Eden","Grafton","Grey Lynn"] and ["Glen Innes","Golflands","Goodwood Heights"] and ["Glendene","Glendowie","Greenhithe","Greenlane"] and ["Glenfield"] and ["Green Bay"] and ["Half Moon Bay","Herne Bay"] and ["Hauraki","Homai"] and ["Henderson"] and ["Herald Island"] and ["Highland Park","Huntington Park","Howick","Hillpark","Highbrook"] and ["Hillcrest"] and ["Hillsborough"] and ["Hingaia"] and ["Hobsonville"] and ["Kaurilands"] and ["Kelston"] and ["Keri Hill"] and ["Kingsland"] and ["Kohimarama"] and ["Konini"] and ["Laingholm"] and ["Lincoln"] and ["Long Bay"] and ["Lucas Heights"] and ["Lynfield"] and ["Mairangi Bay","Mechanics Bay","Mellons Bay","Massey","Mission Bay","Murrays Bay"] and ["Mangere","Mangere Bridge","Middlemore","Morningside"] and ["Mangere East","Mount Albert"] and ["Manukau"] and ["Manurewa"] and ["McLaren Park","Meadowbank"] and ["Milford"] and ["Mission Heights"] and ["Mount Eden","Mount Wellington"] and ["Mount Roskill"] and ["Narrow Neck","Northpark"] and ["New Windsor"] and ["Newmarket"] and ["Newton", "New Lynn"] and ["Northcote"] and ["Northcross"] and ["One Tree Hill"] and ["Opaheke"] and ["Orakei"] and ["Otahuhu"] and ["Onehunga","Oranga","Otara","Oteha","Owairaka"] and ["Pahurehure","Penrose","Papakura","Pakuranga","Panmure","Papatoetoe"] and ["Pakuranga Heights"] and ["Pinehill","Parnell"] and ["Point Chevalier"] and ["Point England"] and ["Ponsonby"] and ["Randwick Park","Rosebank","Royal Oak"] and ["Ranui"] and ["Red Hill","Rosehill"] and ["Remuera"] and ["Rosedale"] and ["Rothesay Bay"] and ["Royal Heights"] and ["Saint Heliers"] and ["Saint Marys Bay"] and ["Sandringham"] and ["Schnapper Rock","Shamrock Park","Shelly Park"] and ["Somerville","Sunnyvale"] and ["Southdown","Swanson"] and ["St Johns""St Lukes","Stonefields","Sunnyhills"] and ["Sunnynook"] and ["Stanley Point"] and ["Takanini","Tamaki","Titirangi"] and ["Takapuna","Te Atatu Peninsula","Te Papapa"] and ["Te Atatu South"] and ["Three Kings","Torbay Heights","Totara Heights"] and ["Torbay"] and ["Totara Vale"] and ["Unsworth Heights"] and ["Viaduct Harbour"] and ["Wai o Taiki Bay","Wairau Valley","Wesley"] and ["Waiata Shores","Wattle Downs","Western Heights","Western Springs"] and ["Waima"] and ["Waikowhai","Whenuapai","Wiri"] and ["Waterview"] and ["West Harbour","Wynyard Quarter"] and ["Westfield"] and ["Westgate","Westmere","Waiake"] and ["Weymouth"] and ["Windsor Park","Woodlands Park"]
+Starts_with_A_and_ends_with_Y = ["Albany"]
+Starts_with_A_and_ends_with_N = ["Alfriston"]
+Starts_with_A_and_ends_with_L = ["Arch Hill"]
+Starts_with_A_and_ends_with_D = "Auckland CBD"
+Starts_with_A_and_ends_with_E = ["Avondale"]
 
+Starts_with_B_and_ends_with_L8_ = ["Balmoral"]
+Starts_with_B_and_ends_with_R_ = ["Bayswater"]
+Starts_with_B_and_ends_with_W = ["Bayview"]
+Starts_with_B_and_ends_with_N_ = ["Beach Haven"]
+Starts_with_B_and_ends_with_T_ = ["Belmont"]
+Starts_with_B_and_ends_with_E_ = ["Birkdale"]
+Starts_with_B_and_ends_with_D_ = ["Birkenhead", "Burswood"]
+Starts_with_B_and_ends_with_Y_ = ["Blockhouse Bay", "Browns Bay"]
+Starts_with_B_and_ends_with_D_ = ["Botany Downs"]
+Starts_with_B_and_ends_with_Y_ = ["Botany"]
+Starts_with_B_and_ends_with_H_ = ["Bucklands Beach"]
+
+Starts_with_C_and_ends_with_Y_ = ["Campbells Bay","Castor Bay", "Cockle Bay"]
+Starts_with_C_and_ends_with_D_ = ["Chatswood"]
+Starts_with_C_and_ends_with_K_ = ["Clendon Park","Clover Park"]
+Starts_with_C_and_ends_with_E_ = ["Conifer Grove"]
+
+Starts_with_D_and_ends_with_A_ = ["Dannemora"]
+Starts_with_D_and_ends_with_T_ = ["Devonport"]
+Starts_with_D_and_ends_with_Y_ = ["Drury"]
+
+Starts_with_E_and_ends_with_I_ = ["East Tamaki"]
+Starts_with_E_and_ends_with_S_ = [ "East Tamaki Heights"]
+Starts_with_E_and_ends_with_H_ = ["Eastern Beach"]
+Starts_with_E_and_ends_with_E_ = ["Eden Terrace","Ellerslie"]
+Starts_with_E_and_ends_with_Y_ = ["Eden Valley"]
+Starts_with_E_and_ends_with_M_ = ["Epsom"]
+
+Starts_with_F_and_ends_with_S_ = ["Fairview Heights"]
+Starts_with_F_and_ends_with_E_ = ["Farm Cove"]
+Starts_with_F_and_ends_with_A_ = ["Favona"]
+Starts_with_F_and_ends_with_H_ = ["Flat Bush"]
+Starts_with_F_and_ends_with_L_ = ["Forrest Hill"]
+Starts_with_F_and_ends_with_Y_ = ["Freemans Bay"]
+
+Starts_with_G_and_ends_with_S_ = ["Gardens"]
+Starts_with_G_and_ends_with_N_ = ["Glen Eden","Grafton","Grey Lynn"]
+Starts_with_G_and_ends_with_S_ = ["Glen Innes","Golflands","Goodwood Heights"]
+Starts_with_G_and_ends_with_E_ = ["Glendene","Glendowie","Greenhithe","Greenlane"]
+Starts_with_G_and_ends_with_D_ = ["Glenfield"]
+Starts_with_G_and_ends_with_Y_ = ["Green Bay"]
+
+Starts_with_H_and_ends_with_Y_ = ["Half Moon Bay","Herne Bay"]
+Starts_with_H_and_ends_with_I_ = ["Hauraki","Homai"]
+Starts_with_H_and_ends_with_N_ = ["Henderson"]
+Starts_with_H_and_ends_with_D_ = ["Herald Island"]
+Starts_with_H_and_ends_with_K_ = ["Highland Park","Huntington Park","Howick","Hillpark","Highbrook"]
+Starts_with_H_and_ends_with_T_ = ["Hillcrest"]
+Starts_with_H_and_ends_with_H_ = ["Hillsborough"]
+Starts_with_H_and_ends_with_A_ = ["Hingaia"]
+Starts_with_H_and_ends_with_E_ = ["Hobsonville"]
+
+Starts_with_K_and_ends_with_S_ = ["Kaurilands"]
+Starts_with_K_and_ends_with_N_ = ["Kelston"]
+Starts_with_K_and_ends_with_L_ = ["Keri Hill"]
+Starts_with_K_and_ends_with_D_ = ["Kingsland"]
+Starts_with_K_and_ends_with_A_ = ["Kohimarama"]
+Starts_with_K_and_ends_with_I_ = ["Konini"]
+
+Starts_with_L_and_ends_with_M_ = ["Laingholm"]
+Starts_with_L_and_ends_with_N_ = ["Lincoln"]
+Starts_with_L_and_ends_with_Y_ = ["Long Bay"]
+Starts_with_L_and_ends_with_S_ = ["Lucas Heights"]
+Starts_with_L_and_ends_with_D_ = ["Lynfield"]
+
+Starts_with_M_and_ends_with_Y_ = ["Mairangi Bay","Mechanics Bay","Mellons Bay","Massey","Mission Bay","Murrays Bay"]
+Starts_with_M_and_ends_with_E_ = ["Mangere","Mangere Bridge","Middlemore","Morningside"]
+Starts_with_M_and_ends_with_T_ = ["Mangere East","Mount Albert"]
+Starts_with_M_and_ends_with_U_ = ["Manukau"]
+Starts_with_M_and_ends_with_A_ = ["Manurewa"]
+Starts_with_M_and_ends_with_K_ = ["McLaren Park","Meadowbank"]
+Starts_with_M_and_ends_with_D_ = ["Milford"]
+Starts_with_M_and_ends_with_S_ = ["Mission Heights"]
+Starts_with_M_and_ends_with_N_ = ["Mount Eden","Mount Wellington"]
+Starts_with_M_and_ends_with_L_ = ["Mount Roskill"]
+
+Starts_with_N_and_ends_with_K_ = ["Narrow Neck","Northpark"]
+Starts_with_N_and_ends_with_R_ = ["New Windsor"]
+Starts_with_N_and_ends_with_T_ = ["Newmarket"]
+Starts_with_N_and_ends_with_N_ = ["Newton", "New Lynn"]
+Starts_with_N_and_ends_with_E_ = ["Northcote"]
+Starts_with_N_and_ends_with_S_ = ["Northcross"]
+
+Starts_with_O_and_ends_with_L_ = ["One Tree Hill"]
+Starts_with_O_and_ends_with_E_ = ["Opaheke"]
+Starts_with_O_and_ends_with_I_ = ["Orakei"]
+Starts_with_O_and_ends_with_U_ = ["Otahuhu"]
+Starts_with_O_and_ends_with_A_ = ["Onehunga","Oranga","Otara","Oteha","Owairaka"]
+
+Starts_with_P_and_ends_with_E_ = ["Pahurehure","Penrose","Papakura","Pakuranga","Panmure","Papatoetoe"]
+Starts_with_P_and_ends_with_S_ = ["Pakuranga Heights"]
+Starts_with_P_and_ends_with_L_ = ["Pinehill","Parnell"]
+Starts_with_P_and_ends_with_R_ = ["Point Chevalier"]
+Starts_with_P_and_ends_with_D_ = ["Point England"]
+Starts_with_P_and_ends_with_Y_ = ["Ponsonby"]
+
+Starts_with_R_and_ends_with_K_ = ["Randwick Park","Rosebank","Royal Oak"]
+Starts_with_R_and_ends_with_I_ = ["Ranui"]
+Starts_with_R_and_ends_with_L_ = ["Red Hill","Rosehill"]
+Starts_with_R_and_ends_with_A_ = ["Remuera"]
+Starts_with_R_and_ends_with_E_ = ["Rosedale"]
+Starts_with_R_and_ends_with_Y_ = ["Rothesay Bay"]
+Starts_with_R_and_ends_with_S_ = ["Royal Heights"]
+
+Starts_with_S_and_ends_with_S_ = "Saint Heliers"
+Starts_with_S_and_ends_with_Y_ = "Saint Marys Bay"
+Starts_with_S_and_ends_with_M_ = "Sandringham"
+Starts_with_S_and_ends_with_K_ = "Schnapper Rock","Shamrock Park","Shelly Park"]
+Starts_with_S_and_ends_with_E_ = "Somerville","Sunnyvale"]
+Starts_with_S_and_ends_with_N_ = "Southdown","Swanson"
+Starts_with_S_and_ends_with_S_ = "St Johns""St Lukes","Stonefields","Sunnyhills"
+Starts_with_S_and_ends_with_K_ = "Sunnynook"
+Starts_with_S_and_ends_with_T_ = "Stanley Point"
+
+Starts_with_T_and_ends_with_I_ = "Takanini","Tamaki","Titirangi"
+Starts_with_T_and_ends_with_H_ = "Takapuna","Te Atatu Peninsula","Te Papapa"
+Starts_with_T_and_ends_with_H_ = "Te Atatu South"
+Starts_with_T_and_ends_with_S_ = "Three Kings","Torbay Heights","Totara Heights"
+Starts_with_T_and_ends_with_Y_ = "Torbay"
+Starts_with_T_and_ends_with_E_ = "Totara Vale"
+
+Starts_with_U_and_ends_with_S = "Unsworth Heights"
+
+Starts_with_V_and_ends_with_R_ = "Viaduct Harbour"
+
+Starts_with_W_and_ends_with_Y_ = "Wai o Taiki Bay","Wairau Valley","Wesley"
+Starts_with_W_and_ends_with_S_ = "Waiata Shores","Wattle Downs","Western Heights","Western Springs"
+Starts_with_W_and_ends_with_A_ = "Waima"
+Starts_with_W_and_ends_with_I_ = "Waikowhai","Whenuapai","Wiri"
+Starts_with_W_and_ends_with_W_ = "Waterview"
+Starts_with_W_and_ends_with_R_ = "West Harbour","Wynyard Quarter"
+Starts_with_W_and_ends_with_D_ = "Westfield"
+Starts_with_W_and_ends_with_E_ = "Westgate","Westmere","Waiake"
+Starts_with_W_and_ends_with_H_ = "Weymouth"
+Starts_with_W_and_ends_with_K_ = "Windsor Park","Woodlands Park"
 # Final code of Task 1 
 intro()
 rule()
 rule_1()
-# The main games starts from here
+# The main games starts from here and continues
 # Task 2 
 Ask_User = input("Do you want to start the game ?")
-if Ask_User ==" no" or "no" or "No" or " No" or "NO":
-    import random
-    print(random.choice(Places_in_Auckland))
-else: 
+if Ask_User == "yes" or " yes" or "YES" or " YES":
     input("Ok, Please start !")
+else: 
+    Ask_User == "no" or " no" or "NO" or " NO"
+    print(Starts_with_A_and_ends_with_D) 
 # Task 3 
 # Task 4
+>>>>>>> 757e6fbe09897013b83829a887ea1f4fe56af2a0
